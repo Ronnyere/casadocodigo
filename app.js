fdaed5c49d580546815c35552e6ff2d13083b202
Ronny Andrade <bd.infortec@gmail.com>
2017-02-10 12:31:07 -0200

    INstalando EJS no pojeto NodeJs com EXPRESS

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require ('express');
 var app = express();
 
+app.get('/produtos',function(req,res){
+     res.send("<html><body><h1>Hello Word</h1></body></html>");
+});
+
 app.listen(3000,function(){
 	console.log("Teste de conexão");
 });
